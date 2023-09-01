5543a75d05c3bb31fe49583ff9b66e9c145e1a10
Ney179 <fernando.eng.comp2019@gmail.com>
2023-09-01 08:56:09 -0300

    Entrega do projeto dio

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+
+public class ParametrosInvalidosException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
